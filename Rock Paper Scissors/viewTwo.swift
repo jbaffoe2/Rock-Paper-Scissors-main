21bb8c6c1b4afffd8bd3c81ad45ab5f2a6fb85ce
jbaffoe2 <joe.baffoe@gmail.com>
2022-12-02 10:37:04 -0700

    Initial Commit

---
+++ Rock Paper Scissors/viewTwo.swift
@@ -0,0 +1,37 @@
+//
+//  viewTwo.swift
+//  Rock Paper Scissors
+//
+//  Created by Huber, Jakob - Student on 11/18/22.
+//
+
+import UIKit
+
+class viewTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    var receivingString = ""
+    var receivingInteger = 0
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
